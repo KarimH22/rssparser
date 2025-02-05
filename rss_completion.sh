7f2a1af3a7f029c4a29c180c26f01b26b91d599e
Karim H22 <ingenieur@ingenieur-high-tech.com>
2025-02-05 22:41:45 +0100

    fix completion to be able to complete with path file (option -f)

--- rss_completion.sh
+++ rss_completion.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+_my_completion() {
+  local cur
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  
+  # Generate file and directory paths
+  COMPREPLY=( $(compgen -W "${commands}" -f -- "$cur") )
+}
+
 location_bin=$(which rss.py)
 [ -z "${location_bin}" ] && [ -f ./rss.py ] && location_bin="./rss.py"
 commands=$( grep add_argument ${location_bin} | awk -F'(' '{print $2}' | tr -d ["'"] | tr [','] ['\n'] | awk '/^-/ {print $0}' | xargs )
 [ -z "${commands}" ]  && return
-complete -W "${commands}" rss.py
-complete -W "${commands}" ./rss.py
-complete -W "${commands}" python rss.py
+complete -F _my_completion  rss.py
+complete -F _my_completion ./rss.py
+complete -F _my_completion python rss.py
 echo "rss.py will be completed with ${commands}"
